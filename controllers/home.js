cb741a600f9a0cd435600355d2a29232d7d2126d
Tucker Craig <tuckercraig@Tuckers-Mac-mini.local>
2021-02-04 23:06:51 -0500

    took out an underscore

--- controllers/home.js
+++ controllers/home.js
@@ -14,7 +14,7 @@ const show = function(req, res) {
 
 const delPost = function(req, res, next) {
     console.log('req.user.posts', req.user.posts);
-    req.user.posts._id(req.params.id).remove();
+    req.user.posts.id(req.params.id).remove();
     req.user.save(function(err) {
         res.redirect('/');
     });
